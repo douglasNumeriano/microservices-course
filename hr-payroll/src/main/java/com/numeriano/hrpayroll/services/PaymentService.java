e3bdc8e61aab7fce267d5600f21fe60649b51d77
numeriano <douglas_rnmeriano@hotmail.com>
2022-01-16 18:57:28 -0300

    Feign client

--- hr-payroll/src/main/java/com/numeriano/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/numeriano/hrpayroll/services/PaymentService.java
@@ -2,6 +2,7 @@ package com.numeriano.hrpayroll.services;
 
 import com.numeriano.hrpayroll.entities.Payment;
 import com.numeriano.hrpayroll.entities.Worker;
+import com.numeriano.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,18 +14,22 @@ import java.util.Map;
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
+//    @Value("${hr-worker.host}")
+//    private String workerHost;
+
+//    @Autowired
+//    private RestTemplate restTemplate;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
 
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workerId));
+//        Map<String, String> uriVariables = new HashMap<>();
+//        uriVariables.put("id", String.valueOf(workerId));
 //        requisição feita entre microsserviços
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+//        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
