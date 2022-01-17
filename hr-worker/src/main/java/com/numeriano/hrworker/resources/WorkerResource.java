2eefa450cb9045467a38d80e2f384e7167560aaf
numeriano <douglas_rnmeriano@hotmail.com>
2022-01-17 10:56:32 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/com/numeriano/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/numeriano/hrworker/resources/WorkerResource.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import com.numeriano.hrworker.entities.Worker;
 import com.numeriano.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+
+    @Autowired
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Autowired
+    private Environment environment;
 
     @Autowired
     private WorkerRepository repository;
@@ -26,6 +35,9 @@ public class WorkerResource {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        logger.info("PORT = " + environment.getProperty("local.server.port"));
+
         Worker obj = repository.findById(id).get();
         return ResponseEntity.ok(obj);
     }
