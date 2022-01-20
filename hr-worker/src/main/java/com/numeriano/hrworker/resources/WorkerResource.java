2b9820fc61b95fcb73df34d84bba931cc177cfbf
numeriano <douglas_rnmeriano@hotmail.com>
2022-01-20 15:02:43 -0300

    Private Git repository

--- hr-worker/src/main/java/com/numeriano/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/numeriano/hrworker/resources/WorkerResource.java
@@ -23,8 +23,8 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
-    @Value("${test.config}")
-    private String testConfig;
+//    @Value("${test.config}")
+//    private String testConfig;
 
     @Autowired
     private Environment environment;
@@ -34,7 +34,7 @@ public class WorkerResource {
 
     @GetMapping(value = "/configs")
     public ResponseEntity<Void> getConfigs() {
-        logger.info("CONFIG = " + testConfig);
+        logger.info("CONFIG = deu certo");
         return ResponseEntity.noContent().build();
     }
 
