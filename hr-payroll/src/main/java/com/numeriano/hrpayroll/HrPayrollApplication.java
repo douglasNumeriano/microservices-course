6892e1b063a464720cfba0eef1015bc26459442a
numeriano <douglas_rnmeriano@hotmail.com>
2022-01-20 10:20:55 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/com/numeriano/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/numeriano/hrpayroll/HrPayrollApplication.java
@@ -2,10 +2,13 @@ package com.numeriano.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+//Anotaçõo necessário para uso do Hystrix
+@EnableCircuitBreaker
 @EnableEurekaClient
 //@RibbonClient(name = "hr-worker")
 @EnableFeignClients
--- hr-payroll/src/main/java/com/numeriano/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/com/numeriano/hrpayroll/resources/PaymentResource.java
@@ -1,5 +1,6 @@
 package com.numeriano.hrpayroll.resources;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.numeriano.hrpayroll.entities.Payment;
 import com.numeriano.hrpayroll.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +17,16 @@ public class PaymentResource {
     @Autowired
     private PaymentService service;
 
+
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping(value = "/{workerId}/days/{days}")
     public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
         Payment payment = service.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+    public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+        Payment payment = new Payment("Brann", 400.0, days);
         return ResponseEntity.ok(payment);
     }
 }
