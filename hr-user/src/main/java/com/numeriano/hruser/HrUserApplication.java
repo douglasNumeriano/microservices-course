82be35fb6969de80da059f11521118a887818720
numeriano <douglas_rnmeriano@hotmail.com>
2022-01-24 11:37:14 -0300

    Database seeding

--- hr-user/src/main/java/com/numeriano/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/numeriano/hruser/HrUserApplication.java
@@ -1,15 +1,27 @@
 package com.numeriano.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner {
+
+	// 	Configuração de incriptação
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
 
+	// 	Configuração de incriptação
+	@Override
+	public void run(String... args) throws Exception {
+//		System.out.println("BCRYPT= " + passwordEncoder.encode("123456"));
+	}
 }
