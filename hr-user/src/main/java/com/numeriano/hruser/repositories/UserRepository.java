14ab44a9bd22db8a2186f213f26917c43980dfe0
numeriano <douglas_rnmeriano@hotmail.com>
2022-01-26 09:33:18 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/numeriano/hruser/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.numeriano.hruser.repositories;
+
+import com.numeriano.hruser.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+}
