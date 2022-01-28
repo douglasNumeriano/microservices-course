58aac6c424bc619216c46d8dbcf7e7c7791c0d90
numeriano <douglas_rnmeriano@hotmail.com>
2022-01-28 11:46:13 -0300

    Security configs to config server

--- hr-oauth/src/main/java/com/numeriano/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/numeriano/hroauth/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.numeriano.hroauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,16 +10,19 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig {
 
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
-//    Beans para trabalhar com JWT
+//  Beans para trabalhar com JWT
     @Bean
     public JwtAccessTokenConverter accessTokenConverter(){
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey("MY-SECRET-KEY");
+        tokenConverter.setSigningKey(jwtSecret);
         return tokenConverter;
     }
 
